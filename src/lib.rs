0a613c0ff3372138d128cc3d29c8faf0def45865
Pjottos <->
2020-11-10 22:26:50 +0100

    dramatic optimization and actually use weights

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,15 @@ extern crate test;
 #[macro_export]
 macro_rules! binary_nn {
     ($Name:ident, $InputCount:expr, $HiddenCount:expr, $OutputCount:expr) => {
-        sa::const_assert_eq!($InputCount % 32, 0);
-        sa::const_assert_eq!($HiddenCount % 32, 0);
-        sa::const_assert_eq!($OutputCount % 32, 0);
+        // it's possible to use u32 to provide more flexibility for input/output count
+        // but that results in roughly a 2x slowdown
+        sa::const_assert_eq!($InputCount % 64, 0);
+        sa::const_assert_eq!($HiddenCount % 64, 0);
+        sa::const_assert_eq!($OutputCount % 64, 0);
 
         struct $Name {
-            weights: [u32; Self::WEIGHT_COUNT / 32],
-            values: [u32; Self::WEIGHT_COUNT / 32],
+            weights: [u64; Self::WEIGHT_COUNT / 64],
+            values: [u64; Self::WEIGHT_COUNT / 64],
             act: [u8; Self::NEURON_COUNT],
         }
 
@@ -30,7 +32,7 @@ macro_rules! binary_nn {
     
                     rng.fill_bytes(std::slice::from_raw_parts_mut(
                         result.weights.as_mut_ptr() as *mut u8,
-                        ($InputCount * $HiddenCount) / (32 / 8)
+                        ($InputCount * $HiddenCount) / (64 / 8)
                     ));
                     rng.fill_bytes(&mut result.act);
 
@@ -39,36 +41,36 @@ macro_rules! binary_nn {
             }
 
             #[inline]
-            pub fn cycle(&mut self, input: &[u8; $InputCount]) -> [bool; $OutputCount]{
+            pub fn cycle(&mut self, input: &[u8; $InputCount]) -> [bool; $OutputCount] {
                 let mut output = [false; $OutputCount];
 
-                for i in 0..Self::NEURON_COUNT {
-                    let mut sum = self.calc_sum(i);
-                    if i < $InputCount {
-                        sum += input[i] as i32;
-                    }
-                    
-                    let fire = sum >= self.act[i] as i32;
-                    for j in 0..Self::NEURON_COUNT {
-                        let idx = j * (Self::NEURON_COUNT / 32) + i / 32;
-                        let mask = 1 << (i % 32);
+                for i in (0..Self::NEURON_COUNT).step_by(64) {
+                    // write the values of each connection in batches to reduce memory i/o
+                    // this provides a dramatic speedup of about 133x over setting each bit individually
+                    // (tested with a 64x4096x64 network)
+                    let mut batch: u64 = 0;
 
-                        // TODO: roughly 98% of cycles is spent on the 'or' and 'xor' here, find a way to optimize it
-                        // it seems to be caused by the memory writes
-
-                        // this makes it so the 'or' is never done if not needed
-                        // since the loop is unrolled, there is no overhead from the comparison
-                        if fire {
-                            self.values[idx] |= mask;
-                        } else {
-                            self.values[idx] ^= mask;
+                    for j in i..i + 64 {
+                        let mut sum = self.calc_sum(j);
+                        if j < $InputCount {
+                            sum += input[j] as i32;
+                        }    
+                        let fire = sum >= self.act[j] as i32;
+                        // set bits in the batch variable in the order it should have 
+                        // in the value matrix 
+                        batch |= (fire as u64) << (j - i);
+                        
+                        let output_start = Self::NEURON_COUNT - $OutputCount;
+                        if j > output_start && fire {
+                            let idx = j - output_start;
+                            output[idx] = true;
                         }
                     }
+                    
+                    for j in (0..Self::NEURON_COUNT).step_by(64) {
+                        let idx = j * (Self::NEURON_COUNT / 64) + i / 64;
 
-                    let output_start = Self::NEURON_COUNT - $OutputCount;
-                    if i > output_start && fire {
-                        let idx = i - output_start;
-                        output[idx] = true;
+                        self.values[idx] = batch;
                     }
                 }
                 
@@ -78,10 +80,19 @@ macro_rules! binary_nn {
             #[inline]
             fn calc_sum(&self, neuron: usize) -> i32 {
                 let mut sum = 0;
-                for j in 0..Self::NEURON_COUNT / 32 {
-                    let idx = (neuron * Self::NEURON_COUNT) / 32 + j;
-                    sum += self.values[idx].count_ones() as i32;
-                    sum -= self.values[idx].count_zeros() as i32;
+                for j in 0..Self::NEURON_COUNT / 64 {
+                    let idx = neuron * (Self::NEURON_COUNT / 64) + j;
+                    // perform an xnor with the weights and values, then add the popcount of that value
+                    // and subtract the zero count
+                    // this produces the following value per connection/weight:
+                    //          !weight   weight  
+                    // !value |    1    |  -1    |
+                    // -------+---------+--------+
+                    // value  |   -1    |   1    |
+
+                    let weighted = !(self.weights[idx] ^ self.values[idx]);
+                    sum += weighted.count_ones() as i32;
+                    sum -= weighted.count_zeros() as i32;
                 }
 
                 sum
@@ -91,7 +102,7 @@ macro_rules! binary_nn {
 }
 
 #[cfg(test)]
-mod tests {
+mod benches {
     use test::{Bencher};
     use rand_xoshiro::rand_core::{SeedableRng, RngCore};
     use rand_xoshiro::Xoshiro256StarStar;
