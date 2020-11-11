9a7b0cc66ea43017fad7cd12768f5401bf02fe60
Pjottos <->
2020-11-11 15:48:59 +0100

    switch to vector for weight storage
    this imposes a slight performance degredation but makes the code cleaner
    
    also changed the name and bumped version

--- src/lib.rs
+++ src/lib.rs
@@ -1,160 +1,145 @@
 #![feature(test)]
 
-extern crate static_assertions as sa;
 extern crate test;
 
-#[macro_export]
-macro_rules! binary_nn {
-    ($Name:ident, $InputCount:expr, $HiddenCount:expr, $OutputCount:expr) => {
-        // needed because of the way weights/values are stored
-        sa::const_assert_eq!($Name::NEURON_COUNT % 64, 0);
+use std::iter::repeat_with;
+use rand_xoshiro::rand_core::{RngCore};
 
-        struct $Name {
-            weights: [u64; Self::WEIGHT_COUNT / 64],
-            values: [u64; Self::WEIGHT_COUNT / 64],
-            act: [u8; Self::NEURON_COUNT],
+pub struct BinaryBrain {
+    weights: Vec<u64>,
+    values: Vec<u64>,
+    act: Vec<u8>,
+    input_count: usize,
+    output_count: usize,
+}
+
+impl BinaryBrain {
+    pub fn new(input_count: usize, output_count: usize, total_count: usize, rng: &mut impl RngCore) -> BinaryBrain {
+        let mut act = vec![0; total_count];
+        rng.fill_bytes(act.as_mut_slice());
+
+        let weight_count = total_count * total_count;
+
+        BinaryBrain {
+            weights: repeat_with(|| rng.next_u64()).take(weight_count / 64).collect(),
+            values: repeat_with(|| rng.next_u64()).take(weight_count / 64).collect(),
+            act: act,
+            input_count: input_count,
+            output_count: output_count,
         }
+    }
 
-        impl $Name {
-            const NEURON_COUNT: usize = $InputCount + $HiddenCount + $OutputCount;
-            const WEIGHT_COUNT: usize = Self::NEURON_COUNT * Self::NEURON_COUNT;
+    #[inline]
+    pub fn cycle(&mut self, input: &[u8]) -> Vec<(bool, i32)> {
+        let mut output = vec![(false, 0); self.output_count];
+        let neuron_count = self.act.len();
+        for i in (0..neuron_count).step_by(64) {
+            // write the values of each connection in batches to reduce memory i/o
+            // this provides a dramatic speedup of about 133x over setting each bit individually
+            // (tested with a 64x4096x64 network)
+            let mut batch: u64 = 0;
 
-            pub fn new(rng: &mut impl RngCore) -> $Name {
-                unsafe {
-                    let mut result = $Name {
-                        weights: MaybeUninit::uninit().assume_init(),
-                        act: MaybeUninit::uninit().assume_init(),
-                        values: MaybeUninit::zeroed().assume_init(),
-                    };
-    
-                    rng.fill_bytes(std::slice::from_raw_parts_mut(
-                        result.weights.as_mut_ptr() as *mut u8,
-                        ($InputCount * $HiddenCount) / (64 / 8)
-                    ));
-                    rng.fill_bytes(&mut result.act);
-
-                    result
-                }
-            }
-
-            #[inline]
-            pub fn cycle(&mut self, input: &[u8; $InputCount]) -> [(bool, i32); $OutputCount] {
-                let mut output = [(false, 0); $OutputCount];
-
-                for i in (0..Self::NEURON_COUNT).step_by(64) {
-                    // write the values of each connection in batches to reduce memory i/o
-                    // this provides a dramatic speedup of about 133x over setting each bit individually
-                    // (tested with a 64x4096x64 network)
-                    let mut batch: u64 = 0;
-
-                    for j in i..i + 64 {
-                        let mut sum = self.calc_sum(j);
-                        
-                        let output_start = Self::NEURON_COUNT - $OutputCount;
-                        if j < $InputCount {
-                            sum += input[j] as i32;
-                        } 
-                        let fire = sum >= self.act[j] as i32;
-                        // set bits in the batch variable in the order it should have 
-                        // in the value matrix 
-                        batch |= (fire as u64) << (j - i);
-                        
-                        if j > output_start {
-                            let idx = j - output_start;
-                            output[idx] = (fire, sum);
-                        }
-                    }
-                    
-                    for j in (0..Self::NEURON_COUNT).step_by(64) {
-                        let idx = j * (Self::NEURON_COUNT / 64) + i / 64;
-
-                        self.values[idx] = batch;
-                    }
-                }
+            for j in i..i + 64 {
+                let mut sum = self.calc_sum(j);
                 
-                output
-            }
-
-            #[inline]
-            fn calc_sum(&self, neuron: usize) -> i32 {
-                let mut sum = 0;
-                for j in 0..Self::NEURON_COUNT / 64 {
-                    let idx = neuron * (Self::NEURON_COUNT / 64) + j;
-                    // perform an xnor with the weights and values, then add the popcount of that value
-                    // and subtract the zero count
-                    // this produces the following value per connection/weight:
-                    //          !weight   weight  
-                    // !value |    1    |  -1    |
-                    // -------+---------+--------+
-                    // value  |   -1    |   1    |
-
-                    let weighted = !(self.weights[idx] ^ self.values[idx]);
-                    sum += weighted.count_ones() as i32;
-                    sum -= weighted.count_zeros() as i32;
+                if j < self.input_count {
+                    sum += input[j] as i32;
+                } 
+                let fire = sum >= self.act[j] as i32;
+                // set bits in the batch variable in the order it should have 
+                // in the value matrix 
+                batch |= (fire as u64) << (j - i);
+                
+                let output_start = neuron_count - self.output_count;
+                if j > output_start {
+                    let idx = j - output_start;
+                    output[idx] = (fire, sum);
                 }
-
-                sum
+            }
+            
+            for j in (0..neuron_count).step_by(64) {
+                let idx = j * (neuron_count / 64) + i / 64;
+                self.values[idx] = batch;
             }
         }
-    };
+        
+        output
+    }
+
+    #[inline]
+    fn calc_sum(&self, neuron: usize) -> i32 {
+        let mut sum = 0;
+        let weight_row_size = self.act.len() / 64;
+        let start = neuron * weight_row_size;
+
+        for j in start..start + weight_row_size {
+            // perform an xnor with the weights and values, then add the popcount of that value
+            // and subtract the zero count
+            // this produces the following value per connection/weight:
+            //          !weight   weight  
+            // !value |    1    |  -1    |
+            // -------+---------+--------+
+            // value  |   -1    |   1    |
+
+            // couldn't get the bounds check optimized away unfortunately
+            let weighted = unsafe { !(self.weights.get_unchecked(j) ^ self.values.get_unchecked(j)) };
+            sum += weighted.count_ones() as i32;
+            sum -= weighted.count_zeros() as i32;
+        }
+
+        sum
+    }
 }
 
 #[cfg(test)]
 mod benches {
     use test::{Bencher};
-    use rand_xoshiro::rand_core::{SeedableRng, RngCore};
+    use rand_xoshiro::rand_core::{SeedableRng};
     use rand_xoshiro::Xoshiro256StarStar;
-    use std::mem::MaybeUninit;
-    
-    
-    mod nn_64_64_64 {
-        use super::*;
+    use crate::BinaryBrain;
 
-        binary_nn!(Net, 64, 64, 64); 
 
-        #[bench]
-        fn cycle(b: &mut Bencher) {
-            let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
-            let mut nn = Net::new(&mut rng);
-            let input = [u8::MAX; 64];
+    #[bench]
+    fn cycle_64_64_64(b: &mut Bencher) {
+        let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
+        let mut nn = BinaryBrain::new(64, 64, 192, &mut rng);
+        let input = [u8::MAX; 64];
 
-            b.iter(|| {
-                nn.cycle(&input)
-            });
-        }
+        b.iter(|| {
+            nn.cycle(&input)
+        });
+    } 
+
+    #[bench]
+    fn cycle_64_512_64(b: &mut Bencher) {
+        let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
+        let mut nn = BinaryBrain::new(64, 64, 640, &mut rng);
+        let input = [u8::MAX; 64];
+
+        b.iter(|| {
+            nn.cycle(&input)
+        });
     }
 
-    mod nn_64_512_64 {
-        use super::*;
+    #[bench]
+    fn cycle_64_4096_64(b: &mut Bencher) {
+        let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
+        let mut nn = BinaryBrain::new(64, 64, 4224, &mut rng);
+        let input = [u8::MAX; 64];
 
-        binary_nn!(Net, 64, 512, 64); 
-
-        #[bench]
-        fn cycle(b: &mut Bencher) {
-            let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
-            let mut nn = Net::new(&mut rng);
-            let input = [u8::MAX; 64];
-
-            b.iter(|| {
-                nn.cycle(&input)
-            });
-        }
+        b.iter(|| {
+            nn.cycle(&input)
+        });
     }
 
-    mod nn_64_4096_64 {
-        use super::*;
+    // #[bench]
+    // fn cycle_64_131072_64(b: &mut Bencher) {
+    //     let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
+    //     let mut nn = BinaryBrain::new(64, 64, 131200, &mut rng);
+    //     let input = [u8::MAX; 64];
 
-        binary_nn!(Net, 64, 4096, 64); 
-
-        #[bench]
-        fn cycle(b: &mut Bencher) {
-            let mut rng = Xoshiro256StarStar::seed_from_u64(1234);
-            let mut nn = Net::new(&mut rng);
-            let input = [u8::MAX; 64];
-
-            b.iter(|| {
-                nn.cycle(&input)
-            });
-        }
-    }
+    //     b.iter(|| {
+    //         nn.cycle(&input)
+    //     });
+    // }
 }
